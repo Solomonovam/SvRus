74a268e0fc38a1e59b1b7963bc6ff59197baefbc
solomon9182 <solomon9182@gmail.com>
2019-12-29 21:33:40 +0300

    Добавьте файлы проекта.

---
+++ SvRus/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SvRus.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SvRus.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Authorize]
+        //public IActionResult Index()
+        //{
+        //    return Content(User.Identity.Name);
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Страница с описанием сайта.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Страница с контактами";
+
+            return View();
+        }
+
+        public IActionResult Gelerey()
+        {
+            ViewData["Message"] = "Сообщение 'Фотогалерея Суверенной России'";
+
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
